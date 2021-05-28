4c6fbdad36b3d44a0b74c8b95f9ce90b8c928813
Mihail19992004 <79575182+Mihail19992004@users.noreply.github.com>
2021-05-28 17:51:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     const APP_KEY= '57661d98cbab6f9cbc19da7f35af1dbe'
     const [recipes, setRecipes] = useState([])
     const [search, setSearch] = useState('')
-    const [query, setQuery] = useState('')
+    const [query, setQuery] = useState('chiken')
 
     const getRecipes = async () => {
         const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
@@ -41,16 +41,22 @@ function App() {
         <form onSubmit={getSearch} className='search-form'>
             <input className='search-bar' value={search} onChange={updateSearch} type='text'/>
         </form>
+        <div className="coocking">
+
         {
             recipes.length === 0 ? (
                 <h1 className='no-found'>Пока пусто</h1>
             ) :(
                 recipes.map(recipe=> (
-                    <Recept  ingredients={recipe.recipe.ingredients} callories={recipe.recipe.calories} title={recipe.recipe.label} key={recipe.recipe.calories} img ={recipe.recipe.image} />
+                    <Recept  ingredients={recipe.recipe.ingredients}
+                             callories={recipe.recipe.calories}
+                             title={recipe.recipe.label}
+                             key={recipe.recipe.calories}
+                             img ={recipe.recipe.image} />
                 ))
             )
         }
-
+        </div>
     </div>
   );
 }
